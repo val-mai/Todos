b224ce4b8334ee944d9d9cf06d6ac8207d2958de
val-mai <valerio.maiolini@gmail.com>
2022-07-01 16:13:34 +0200

    primo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompletedComponent } from './components/completed/completed.component';
+import { ImportantComponent } from './components/important/important.component';
+import { TodosComponent } from './components/todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path : "all",
+  pathMatch : 'full',
+  component : TodosComponent
+},
+{
+  path : "completed",
+  pathMatch : 'full',
+  component : CompletedComponent
+},
+{
+  path : "important",
+  pathMatch : 'full',
+  component : ImportantComponent
+},
+{
+  path: '',
+  redirectTo: '/all',
+  pathMatch: 'full',
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompletedComponent } from './components/completed/completed.component';
+import { InputComponent } from './components/input/input.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { ImportantComponent } from './components/important/important.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompletedComponent,
+    InputComponent,
+    FiltersComponent,
+    TodosComponent,
+    ImportantComponent
   ],
   imports: [
     BrowserModule,
